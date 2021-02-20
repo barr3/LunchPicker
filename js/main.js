6785ed828478c97b6c87613abf9900c8c16bb871
Barre Nils Fredrik Svante Åke Blomberg <fred03.blomberg@gmail.com>
2021-02-20 16:08:50 +0100

    Added sorting
    Added the ability to sort the list of restaurant objects according to distance and alphabet. 
    
    Will soon add the ability to sort after amount of people and amount of food, aswell as the ability to reverse the sorting.

--- js/main.js
+++ js/main.js
@@ -34,6 +34,11 @@ class Restaurang {
         this.createRest();
     }
 
+    get getName(){
+        return this.namn;
+    }
+
+
     //Checks wether the user is at craafords och annexet and sets "avstånd" accordingly
     setAvstånd(annexet){
         if (annexet == true) {
@@ -70,11 +75,102 @@ function toggleAnnexet() {
 
 }
 
-//Constructs falafelkungen
-let falafelKungen = new Restaurang("Falafelkungen", "http://google.com",420, 500, 1000, 9, 9, "Kebab", 7, "./img/falafelkungen", "bild på falafelkungen", "falafel");
-list.push(falafelKungen);
+
+function sortRest(){
+
+    //console.log("sort pressesd");
+    //document.getElementById("0").innerHTML =""; //Clears the html
+
+    console.log(list);
+
+
+    /*
+    var i;
+    for (i = 0;i < list.length; i++) {
+
+        var ascii = list[i].getName.charCodeAt();
+        console.log(list[i].getName);
+        console.log(ascii);
+
+
+
+    }
+
+    */
+
+    /*
+    //Sorterar efter avstånd
+    list.sort((a,b) => {
+
+        return a.avstånd - b.avstånd;
+
+    })
+    */
+
+
+
+
+    list.sort((a,b) => {
+
+        let na = a.namn.toLowerCase(),
+            nb = b.namn.toLowerCase();
+
+        if (na < nb){
+            return -1;
+        }
+        if (na > nb) {
+            return 1;
+        }
+        return 0;
+
+    });
+
+
+
+    //Listar namnen
+    list.forEach((e) => {
+
+        console.log(`${e.namn} ${e.avstånd} `);
+
+    })
+
+
+    console.log(list);
+
+    //return item1.attr.localeCompare(item2.attr);
+
+
+    /*
+    var i;
+    for (i = 0; i < list.length; i++) { //Loops through all the restaurants.
+
+        //console.log(list[i].getName);
+
+
+
+        //console.log(list[i]);
+    }
+
+    */
+
+}
+
+
+
+
 
 
 //Constructs mcdonalds
 let mcdonalds = new Restaurang("Mcdonalds", "http://google.com",690, 250, 600, 9, 9, "Hamburgare", 5, "./img/mcdonalds.jpeg", "bild på mcdonalds", "mcdonalds");
 list.push(mcdonalds);
+
+//Constructs falafelkungen
+let falafelKungen = new Restaurang("Falafelkungen", "http://google.com",420, 500, 1000, 9, 9, "Kebab", 7, "./img/falafelkungen", "bild på falafelkungen", "falafel");
+list.push(falafelKungen);
+
+
+
+let nybergs = new Restaurang("Nybergs Konditori", "http://biltema.se",0, 50, 300, 8, 5, "Konditori", 9, "./img/nybergs", "bild på nybergs koditori", "nybergs");
+list.push(nybergs);
+
+//console.log(list);
