a6a1858691d1148e483487003b806ecb9f7a3331
barr3 <barre@novigrad.localadmin>
2021-03-04 14:21:00 +0100

    Cleaned up and commented code.

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,53 +1,14 @@
 "use strict";
 
-
-//var restauranger = [];
-
 const LOCAL_STORAGE_KEY_NTILH = "app.rests";
 
+//Loads in the restaurant objects already stored in local storage.
 let rests = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NTILH)) || [];
 
-console.log(rests);
-
-
-//remove later
-// class Restaurang {
-
-// 	constructor(namn, adress, avståndAnnexet, avståndCraafords, mat, folk, typ, betyg, img_link, img_alt) {
-
-// 		this.namn = namn; //Namnet på restaurangen
-// 		this.adress = adress; //Address
-
-// 		this.avståndAnnexet = avståndAnnexet;
-// 		this.avståndCraafords = avståndCraafords;
-// 		//this.avstånd  = avstånd; //avståndet från annexet eller Craafords
-
-// 		this.mat = mat; //Hur mycket mat på en skala 1-10
-// 		this.folk = folk; //Hur mycket folk på en skala 1-10
-// 		this.typ = typ; //Vilken typ av mat. Pizza, kebab, husman, et.c
-// 		this.betyg = betyg; //Totalt betyg på en skala 1-10
-// 		// this.favorit = favorit; //Bool om restaurangen är en favorit
-// 		this.img_link = img_link;
-// 		this.img_alt = img_alt;
-// 		this.id = namn;
-
-// 	}
-// }
-
-
-function test() {
-	console.log(restauranger);
-
-	// for (var i = 0; i < restauranger.length; i++) {
-	// 	console.log(i);
-	// 	console.log(restauranger[i]);
-	// }
-
-}
-
-
+//Takes the values put in the HTML form and sends them to local storage. 
 function store() {
 
+	//Takes the values of the fields and assigns them to an appropriate temp variable.
 	var temp_name = document.getElementById("name").value;
 	var temp_adress = document.getElementById("adress").value;
 	var temp_avsAnn = parseInt(document.getElementById("avsAnn").value); //Parse later
@@ -58,82 +19,22 @@ function store() {
 	var temp_betyg = document.getElementById("betyg").value;
 	var temp_imgLänk = document.getElementById("imgLänk").value;
 	var temp_imgAlt = document.getElementById("imgAlt").value;
-	
 
-	// restauranger.push(new Restaurang(namn, adress, avsAnn, avsCra, mat, folk, typ, betyg, imgLänk, imgAlt));
-	//localStorage.setItem(test.namn, JSON.stringify(test));
-
+	//Creates a object with the values given. 
 	var obj = { name: temp_name, adress: temp_adress, avståndAnnexet: temp_avsAnn, avståndCraafords: temp_avsCra, mat: temp_mat, folk: temp_folk, typ: temp_typ, betyg: temp_betyg, img_link: temp_imgLänk, img_alt: temp_imgAlt };
 
-	// console.log(restMidMan.namn);
-	// var myJSON = JSON.stringify(restMidMan);
-
-	
-	
-	//console.log(JSON.stringify(restMidMan));
-
-	// console.log(namn);
-
-	// var obj = {name : "barre", age: 17, city : "Stockholm"};
-
-
+	//Makes the object a JSON object.
 	var temp_rest = JSON.stringify(obj);
 
+	//Pushes the JSON object to the rests array
 	rests.push(temp_rest);
-
-	console.log(rests);
+	
+	//Pushes the rests array to local storage. 
 	localStorage.setItem(LOCAL_STORAGE_KEY_NTILH, JSON.stringify(rests));
-
-	// console.log(restauranger);
-
-
 	
-	//localStorage.setItem(restauranger, JSON.stringify(restauranger));
-
-	//var korv = localStorage.getItem();
-
-	// let rests = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NTILH)) || [];
-
-	// console.log(rests);
-	
-	// console.log("hej hej");
-	// console.log(korv);
-	
-  
-	// var temp_var = localStorage.getItem(restauranger);
-	// var temp_var2 = JSON.parse(temp_var);
-
-	// console.log(temp_var2);
-
-
-
-
-
-	//Don't know about this
-	// var JSONMiddleMan = '"interChange" : [ '+
-	// 	'{ "namn" : "", "adress" : "", "avsAnn" : "", "avsCra" : "", "mat": "" }'
-
 }
 
-
-// for (var i = 0; i < restauranger.length; ++i) {
-//     console.log(i);
-//     var temp_var = localStorage.getItem(i.name);
-//     var temp_var2 = JSON.parse(temp_var);
-//     console.log("Restaurangen är: ");
-//     console.log(temp_var2);
-
-// } 
-
-
-// function storeRestaurants(){
-// 	console.log(rests);
-
-// 	localStorage.setItem(LOCAL_STORAGE_KEY_NTILH, JSON.stringify(rests));
-	
-// }
-
-
+//Function name pretty much explains itself.
 function clearRestaurants(){
 
 	rests = [];
--- js/main.js
+++ js/main.js
@@ -1,66 +1,13 @@
 "use strict";
 
-
-//Might use local storage later for favorites
-
 const LOCAL_STORAGE_KEY_NTILH = "app.rests";
 
-// let rests = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NTILH)) || [];
 
-// localStorage.setItem("mat", "korv");
-
-// console.log(localStorage.getItem("mat"));
-
-// var korv = {namn : "Barre", stad : "Stockholm"};
-
-// function storeShit(){
-// 	localStorage.setItem(LOCAL_STORAGE_KEY_NTILH, JSON.stringify(korv));
-// }
-
-function getShit() {
-	//Extremely messy code, but it works i guess.
-	let rests = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NTILH)) || [];
-
-	if (rests.length != 0) {
-
-		for (var i = 0; i < rests.length; i++) {
-			var list_temp_var = JSON.parse(rests[i]);
-			console.log(list_temp_var);
-
-			list.push(new Restaurang(list_temp_var.name, list_temp_var.adress,0, list_temp_var.avståndAnnexet, list_temp_var.avståndCraafords, list_temp_var.mat, list_temp_var.folk, list_temp_var.typ, list_temp_var.betyg, list_temp_var.img_link, list_temp_var.img_alt, list_temp_var.name));
-			
-		}
-		
-
-
-		// console.log(list_temp_var);
-
-		// console.log(list_temp_var.name, list_temp_var.adress, 0, list_temp_var.avståndAnnexet, list_temp_var.avståndCraafords, list_temp_var.mat, list_temp_var.folk, list_temp_var.typ, list_temp_var.betyg, list_temp_var.img_link, list_temp_var.img_alt, list_temp_var.name);
-
-	}
-
-	
-
-	// list.push(new Restaurang(list_temp_var.name, list_temp_var.adress,0, list_temp_var.avståndAnnexet, list_temp_var.avståndCraafords, list_temp_var.mat, list_temp_var.folk, list_temp_var.typ, list_temp_var.betyg, list_temp_var.img_link, list_temp_var.img_alt, list_temp_var.name));
-
-
-
-
-	// console.log(rests);
-}
-
-
-
-//var korv = localStorage.getItem("restauranger");
-
-//console.log(korv);
-// console.log(rests);
-
-
-let annexet = false; //Sets annexet to false. i.e, Craafords is the default starting location.
+let annexet = true; //Sets annexet to false. i.e, Craafords is the default starting location.
 var list = []; //List that contains all the restaurant objects
 
-//Restaurant class
+
+//Restaurant class 
 class Restaurang {
 
 	constructor(namn, adress, avstånd, avståndAnnexet, avståndCraafords, mat, folk, typ, betyg, img_link, img_alt, id) {
@@ -77,12 +24,13 @@ class Restaurang {
 		this.typ = typ; //Vilken typ av mat. Pizza, kebab, husman, et.c
 		this.betyg = betyg; //Totalt betyg på en skala 1-10
 		// this.favorit = favorit; //Bool om restaurangen är en favorit
-		this.img_link = img_link;
+		this.img_link = img_link; // 
 		this.img_alt = img_alt;
-		this.id = id;
-		this.createRest();
+		this.id = id; //Should be removed later, the id is essentially the same as name.
+		this.createRest(); //adds the restaurant object to html
 	}
 
+	//getter for getting the object name
 	get getName() {
 		return this.namn;
 	}
@@ -107,7 +55,9 @@ class Restaurang {
 
 }
 
-//Is triggered by the press of the button on the top of the webpage. Toggle the location of the user.
+
+
+//Is triggered by the press of the button on the top of the webpage. Toggles the location of the user between "annexet" and "crafoords"
 function toggleAnnexet() {
 	if (annexet == true) {
 		annexet = false;
@@ -124,7 +74,7 @@ function toggleAnnexet() {
 
 }
 
-
+//Sorts the restaurants depending on what the user chooses
 function sortRest(input) {
 
 	//Removes all the Restaurant code in index.html so it can be created later
@@ -241,118 +191,6 @@ function sortRest(input) {
 
 
 
-	/*
-	//Sorterar efter avstånd med kortast avstånd på toppen
-	list.sort((a,b) => {
-    
-	    return a.avstånd - b.avstånd;
-    
-	})
-	*/
-
-	/*
-	//Sorterar efter avstånd med kortast avstånd på toppen
-	list.sort((a,b) => {
-    
-	    return b.avstånd - a.avstånd;
-    
-	})
-	*/
-
-
-	/*
-	//Sorterar efter antalet människor med minst antal människor på toppen
-	list.sort((a,b) => {
-    
-	    return a.folk - b.folk;
-    
-	})
-	*/
-
-	/*
-	//Sorterar efter antalet människor med flest antal människor på toppen
-	list.sort((a,b) => {
-    
-	    return b.folk - a.folk;
-    
-	})
-	*/
-
-
-	/*
-	//Sorterar med mest mat på toppen
-	list.sort((a,b) => {
-    
-	    return b.mat - a.mat;
-    
-	})
-	*/
-
-	/*
-	//Sorterar med minst mat på toppen
-	list.sort((a,b) => {
-    
-	    return a.mat - b.mat;
-    
-	})
-	*/
-
-
-	/*
-	//Sorterar alfabetiskt med A på toppen
-	list.sort((a,b) => {
-    
-	    let na = a.namn.toLowerCase(),
-		nb = b.namn.toLowerCase();
-    
-	    if (na < nb){
-		return -1;
-	    }
-	    if (na > nb) {
-		return 1;
-	    }
-	    return 0;
-    
-	});
-	*/
-
-	/*
-	//Sorterar alfabetiskt med A på toppen
-	list.sort((a,b) => {
-    
-	    let na = a.namn.toLowerCase(),
-		nb = b.namn.toLowerCase();
-    
-	    if (na < nb){
-		return 1;
-	    }
-	    if (na > nb) {
-		return -1;
-	    }
-	    return 0;
-    
-	});
-	*/
-
-
-	/////////////////////////////////////////
-
-	//Listar namnen
-
-	/*
-	list.forEach((e) => {
-    
-	    console.log(`${e.namn} ${e.namn} `);
-    
-	})
-	*/
-
-
-
-	//return item1.attr.localeCompare(item2.attr);
-
-
-
 	var i;
 	for (i = 0; i < list.length; i++) { //Loops through all the restaurants.
 
@@ -368,14 +206,11 @@ function sortRest(input) {
 
 }
 
-
-
+//Gets called when the user chooses a sorting option. 
 function changed() {
 
 	var sortSelect = document.getElementById("sort");
 	var selected = sortSelect.options[sortSelect.selectedIndex].value;
-
-	console.log(selected);
 
 	if (selected != "def") {
 		sortRest(selected);
@@ -386,57 +221,55 @@ function changed() {
 }
 
 
+//Add all the hardcoded restaurants between the two lines
+//=============================================================================================================================
+
+
 //Constructs mcdonalds
 let mcdonalds = new Restaurang("Mcdonalds", "http://google.com", 690, 250, 600, 6, 9, "Hamburgare", 5, "./img/mcdonalds.jpeg", "bild på mcdonalds", "mcdonalds");
-list.push(mcdonalds);
+list.push(mcdonalds); //Pushes the object to the "list" list
 
 //Constructs falafelkungen
 let falafelKungen = new Restaurang("Falafelkungen", "http://google.com", 420, 500, 1000, 9, 9, "Kebab", 7, "./img/falafelkungen", "bild på falafelkungen", "falafel");
-list.push(falafelKungen);
+list.push(falafelKungen); //Pushes the object to the "list" list
 
 
-
+//Constructs nybergs
 let nybergs = new Restaurang("Nybergs Konditori", "http://biltema.se", 0, 50, 300, 8, 5, "Konditori", 9, "./img/nybergs", "bild på nybergs koditori", "nybergs");
-list.push(nybergs);
+list.push(nybergs); //Pushes the object to the "list" list
 
+//Constructs subway
 let subway = new Restaurang("Subway", "http://youtube.com", 0, 100, 400, 6, 10, "Mackor", 7, "./img/subway.jpeg", "bild på subway", "subway");
-list.push(subway);
+list.push(subway); //Pushes the object to the "list" list
 
 
 
-//tested localstorage
-// localStorage.setItem("nybergs", JSON.stringify(nybergs));
-// var test = localStorage.getItem("nybergs");
-// var testTest = JSON.parse(test);
-// console.log(testTest);
+//=============================================================================================================================
 
-// storeRestaurant(nybergs);
-// retrieveRestaurant(nybergs);
+//Fixes bug where the wrong distance is displayed if you dont manually pick a location.
+//Should fix later if I have the time. 
+toggleAnnexet();
 
 
-function storeRestaurant(restaurant) {
 
-	localStorage.setItem(restaurant.getName, JSON.stringify(restaurant));
+//Extremely messy code, but it works i guess. Runs when the page is initialized.
+//Adds all restaurant objects in localstorage. 
+let rests = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NTILH)) || [];
+
+if (rests.length != 0) {
+
+	for (var i = 0; i < rests.length; i++) {
+		var list_temp_var = JSON.parse(rests[i]);
+		// console.log(list_temp_var);
+
+		list.push(new Restaurang(list_temp_var.name, list_temp_var.adress, 0, list_temp_var.avståndAnnexet, list_temp_var.avståndCraafords, list_temp_var.mat, list_temp_var.folk, list_temp_var.typ, list_temp_var.betyg, list_temp_var.img_link, list_temp_var.img_alt, list_temp_var.name));
+
+	}
+
+	// console.log(list_temp_var);
+
+	// console.log(list_temp_var.name, list_temp_var.adress, 0, list_temp_var.avståndAnnexet, list_temp_var.avståndCraafords, list_temp_var.mat, list_temp_var.folk, list_temp_var.typ, list_temp_var.betyg, list_temp_var.img_link, list_temp_var.img_alt, list_temp_var.name);
 
 }
 
-function retrieveRestaurant(restaurant) {
 
-	var temp_var1 = localStorage.getItem(restaurant.getName);
-	var temp_var2 = JSON.parse(temp_var1);
-	console.log("Restaurangen är: ");
-	console.log(temp_var2);
-
-	console.log(temp_var2.namn);
-
-}
-
-
-
-
-
-
-
-
-
-//console.log(list);
